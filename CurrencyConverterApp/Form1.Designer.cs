52857e64bbda539a36cfde9bbb94eb676901027a
Mateusz Boczarski <matbocz@outlook.com>
2020-01-16 17:05:03 +0100

    Create basic ui

--- CurrencyConverterApp/Form1.Designer.cs
+++ CurrencyConverterApp/Form1.Designer.cs
@@ -28,13 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.comboBoxFrom = new System.Windows.Forms.ComboBox();
+            this.comboBoxTo = new System.Windows.Forms.ComboBox();
+            this.labelFrom = new System.Windows.Forms.Label();
+            this.labelTo = new System.Windows.Forms.Label();
+            this.pictureBoxFrom = new System.Windows.Forms.PictureBox();
+            this.pictureBoxTo = new System.Windows.Forms.PictureBox();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFrom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelTitle.Location = new System.Drawing.Point(79, 65);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(252, 31);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "Currency Converter";
+            // 
+            // comboBoxFrom
+            // 
+            this.comboBoxFrom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxFrom.FormattingEnabled = true;
+            this.comboBoxFrom.Location = new System.Drawing.Point(12, 160);
+            this.comboBoxFrom.Name = "comboBoxFrom";
+            this.comboBoxFrom.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxFrom.TabIndex = 1;
+            // 
+            // comboBoxTo
+            // 
+            this.comboBoxTo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTo.FormattingEnabled = true;
+            this.comboBoxTo.Location = new System.Drawing.Point(268, 160);
+            this.comboBoxTo.Name = "comboBoxTo";
+            this.comboBoxTo.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxTo.TabIndex = 2;
+            // 
+            // labelFrom
+            // 
+            this.labelFrom.AutoSize = true;
+            this.labelFrom.Location = new System.Drawing.Point(12, 140);
+            this.labelFrom.Name = "labelFrom";
+            this.labelFrom.Size = new System.Drawing.Size(44, 17);
+            this.labelFrom.TabIndex = 3;
+            this.labelFrom.Text = "From:";
+            // 
+            // labelTo
+            // 
+            this.labelTo.AutoSize = true;
+            this.labelTo.Location = new System.Drawing.Point(265, 140);
+            this.labelTo.Name = "labelTo";
+            this.labelTo.Size = new System.Drawing.Size(29, 17);
+            this.labelTo.TabIndex = 4;
+            this.labelTo.Text = "To:";
+            // 
+            // pictureBoxFrom
+            // 
+            this.pictureBoxFrom.Location = new System.Drawing.Point(12, 190);
+            this.pictureBoxFrom.Name = "pictureBoxFrom";
+            this.pictureBoxFrom.Size = new System.Drawing.Size(121, 121);
+            this.pictureBoxFrom.TabIndex = 5;
+            this.pictureBoxFrom.TabStop = false;
+            // 
+            // pictureBoxTo
+            // 
+            this.pictureBoxTo.Location = new System.Drawing.Point(268, 190);
+            this.pictureBoxTo.Name = "pictureBoxTo";
+            this.pictureBoxTo.Size = new System.Drawing.Size(121, 121);
+            this.pictureBoxTo.TabIndex = 6;
+            this.pictureBoxTo.TabStop = false;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelResult.Location = new System.Drawing.Point(300, 314);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(48, 19);
+            this.labelResult.TabIndex = 7;
+            this.labelResult.Text = "label1";
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(268, 363);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(121, 23);
+            this.buttonConvert.TabIndex = 8;
+            this.buttonConvert.Text = "Convert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(401, 398);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.pictureBoxTo);
+            this.Controls.Add(this.pictureBoxFrom);
+            this.Controls.Add(this.labelTo);
+            this.Controls.Add(this.labelFrom);
+            this.Controls.Add(this.comboBoxTo);
+            this.Controls.Add(this.comboBoxFrom);
+            this.Controls.Add(this.labelTitle);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFrom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.ComboBox comboBoxFrom;
+        private System.Windows.Forms.ComboBox comboBoxTo;
+        private System.Windows.Forms.Label labelFrom;
+        private System.Windows.Forms.Label labelTo;
+        private System.Windows.Forms.PictureBox pictureBoxFrom;
+        private System.Windows.Forms.PictureBox pictureBoxTo;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button buttonConvert;
     }
 }
 
